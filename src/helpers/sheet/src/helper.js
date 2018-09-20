cc0377f6b7fba3700692e6f8de5d40ad64f9ed6d
soederpop <jon@dais.com>
2018-09-19 19:52:17 -0500

    support additional provider methods for working with data in the sheet helper

--- src/helpers/sheet/src/helper.js
+++ src/helpers/sheet/src/helper.js
@@ -11,25 +11,56 @@ export function attach(runtime) {
     static strictMode = false
     static google = google
 
-    state = runtime.mobx.observable.shallowMap([['authorized', false]])
+    get data() {
+      return this.state.get('data')
+    }
+
+    get info() {
+      return this.state.get('info')
+    }
+
+    get worksheets() {
+      return this.get('info.worksheets', [])
+    }
 
     async initialize() {
+      this.hide('state', this.runtime.mobx.observable.shallowMap([]))
       this.hide('spreadsheet', this.createSpreadsheet())
+
       await this.authorize()
       await this.getInfo()
 
-      const { pickBy, isFunction } = this.lodash
-
-      try {
-        this.applyInterface(pickBy({ ...this.provider, ...this.options }, v => isFunction(v)), {
-          partial: [],
-          insertOptions: false,
-        })
-      } catch (error) {
-        this.runtime.error(`Error while applying sheet interface`, error.message)
+      if (this.result('eagerLoaded')) {
+        await this.loadAll()
       }
 
+      this.applySheetInterface()
+
       return this.authorized
+    }
+
+    applySheetInterface(iface = this.sheetInterface) {
+      const { isEmpty } = this.lodash
+      if (!isEmpty(iface)) {
+        try {
+          this.applyInterface(iface, {
+            partial: [],
+            insertOptions: false,
+          })
+        } catch (error) {
+          this.runtime.error(`Error while applying sheet interface`, error.message)
+          this.state.set('interfaceError', error)
+        }
+      }
+    }
+
+    get sheetInterface() {
+      const { pickBy, omit, isFunction } = this.lodash
+
+      return omit(
+        pickBy({ ...this.provider, ...this.options }, (v, k) => isFunction(v) && !this.has(k)),
+        'initialize'
+      )
     }
 
     get google() {
@@ -37,20 +68,29 @@ export function attach(runtime) {
     }
 
     async loadAll(options = {}) {
-      const { omit, fromPairs, mapKeys } = this.lodash
+      const { camelCase, kebabCase } = this.runtime.stringUtils
+      const { isEmpty, mapKeys } = this.lodash
 
-      const sanitize = rows =>
-        rows.map(r =>
-          mapKeys(omit(r, 'id', '_xml', '_links', 'app:edited', 'save', 'del'), (v, k) =>
-            k.replace(/-/g, '_')
-          )
-        )
+      if (!this.authorized) {
+        await this.authorize()
+      }
 
-      const results = await Promise.all(
-        this.spreadsheet.worksheets.map(ws => this.getRows(ws).then(r => [ws.title, sanitize(r)]))
-      )
+      if (isEmpty(this.info)) {
+        await this.getInfo()
+      }
 
-      return fromPairs(results)
+      const { receiveData = this.options.receiveData || this.provider.receiveData } = options
+      let rawData = await loadAll.call(this, options)
+
+      rawData = mapKeys(rawData, (v, k) => camelCase(kebabCase(k)))
+
+      if (typeof receiveData === 'function') {
+        rawData = receiveData.call(this, rawData)
+      }
+
+      this.state.set('data', rawData)
+
+      return rawData
     }
 
     createSpreadsheet(options = {}) {
@@ -64,7 +104,7 @@ export function attach(runtime) {
     }
 
     async getInfo() {
-      const info = new Promise((resolve, reject) => {
+      const info = await new Promise((resolve, reject) => {
         this.spreadsheet.getInfo((err, info) => (err ? reject(err) : resolve(info)))
       })
 
@@ -193,4 +233,21 @@ export function attach(runtime) {
   }
 
   return runtime
+}
+
+async function loadAll(options = {}) {
+  const { omit, fromPairs, mapKeys } = this.lodash
+
+  const sanitize = rows =>
+    rows.map(r =>
+      mapKeys(omit(r, 'id', '_xml', '_links', 'app:edited', 'save', 'del'), (v, k) =>
+        k.replace(/-/g, '_')
+      )
+    )
+
+  const results = await Promise.all(
+    this.worksheets.map(ws => this.getRows(ws).then(r => [ws.title, sanitize(r)]))
+  )
+
+  return fromPairs(results)
 }
