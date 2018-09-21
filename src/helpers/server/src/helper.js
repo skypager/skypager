a5994da39f14db94bb8ff539b64d82f75be52439
soederpop <jon@dais.com>
2018-09-21 15:47:58 -0500

    improved server helper lifecycle sequence, added a display banner to the server

--- src/helpers/server/src/helper.js
+++ src/helpers/server/src/helper.js
@@ -23,13 +23,11 @@ export class Server extends Helper {
   createServer(options = {}, context = {}) {
     const {
       appWillMount = this.options.appWillMount || this.provider.appWillMount,
-      appDidMount = this.options.appDidMount || this.provider.appDidMount,
       createServer = this.options.createServer || this.provider.createServer,
     } = options
 
-    let history = this.tryResult('history', () => false)
-    let serveStatic = this.tryResult('serveStatic', () => false)
     let cors = this.tryResult('cors')
+    let pretty = this.tryResult('pretty', () => process.env.NODE_ENV !== 'production')
 
     let app
 
@@ -43,19 +41,43 @@ export class Server extends Helper {
       setupCors.call(this, app, cors)
     }
 
-    if (appWillMount) {
-      appWillMount.call(this, app, options, context)
+    if (pretty) {
+      this.runtime.debug('Enabling pretty printing of JSON responses')
+      app.use(require('express-prettify')({ query: 'pretty' }))
     }
 
+    if (appWillMount) {
+      this.runtime.debug('App Will Mount Hook was provided')
+      appWillMount.call(this, app, options, context)
+      this.runtime.debug('App Will Mount Hook was called')
+    } else {
+      this.runtime.debug('No App Will Mount Hook Was Provided')
+    }
+
+    return app
+  }
+
+  async mount(options = this.options, context = this.context) {
+    const { app } = this
+    const { appDidMount = this.options.appDidMount || this.provider.appDidMount } = options
+    let {
+      history = this.tryResult('history', () => false),
+      serveStatic = this.tryResult('serveStatic', () => false),
+    } = options
+
+    this.runtime.debug('App Did Mount')
+
     if (appDidMount) {
-      appDidMount.call(this, app, options, context)
+      await appDidMount.call(this, app, options, context)
     }
 
     if (serveStatic) {
+      this.runtime.debug('Setting up static file hosting')
       setupStaticServer.call(this, app, serveStatic)
     }
 
     if (history) {
+      this.runtime.debug('Setting up history fallback for single page app hosting')
       setupHistoryFallback.call(this, app, history)
     }
 
@@ -63,30 +85,52 @@ export class Server extends Helper {
   }
 
   async start(...args) {
-    const { app } = this
-    const { debug, error } = this.runtime.logger
-    const { serverDidFail, serverWillStart } = this.provider
+    const {
+      serverDidFail = this.provider.serverDidFail,
+      serverWillStart = this.provider.serverWillStart,
+    } = this.options
+
+    if (!this.stats.get('mounted')) {
+      await this.mount(...args)
+    }
 
     if (serverWillStart) {
+      this.runtime.debug('server will start hook')
       await serverWillStart.call(this, this.options, this.context)
+      this.runtime.debug('server will start hook finished')
     }
 
     try {
+      this.runtime.debug('startServer started')
       await this.startServer(...args)
+      this.runtime.debug('startServer finished')
       this.stats.set('started', true)
     } catch (err) {
       this.stats.set('started', false)
       this.stats.set('failed', true)
-
-      console.error(err)
+      this.runtime.error('startServerFailed')
 
       if (serverDidFail) {
+        this.runtime.debug('server did fail hook')
         await serverDidFail.call(this, { error: err }, this.context)
+        this.runtime.debug('server did fail hook finished')
       }
 
       throw err
     }
 
+    if (this.options.showBanner !== false) {
+      if (this.tryGet('displayBanner')) {
+        this.tryResult('displayBanner')
+      } else {
+        this.runtime.cli.clear()
+        this.runtime.cli.randomBanner(`Skypager`)
+        this.runtime.cli.print(`The Server is started`)
+        this.runtime.cli.print([`Open in your browser:`, `http://${this.hostname}:${this.port}`])
+      }
+    }
+
+    this.stats.set('started', true)
     return this
   }
 
@@ -126,7 +170,7 @@ export class Server extends Helper {
   }
 
   get framework() {
-    return this.tryGet('provider.framework', this.express)
+    return this.tryGet('framework', this.express)
   }
 
   get express() {
@@ -157,15 +201,19 @@ function setupHistoryFallback(app, historyOptions) {
   const history = require('express-history-api-fallback')
 
   if (historyOptions === true) {
+    const htmlFile = runtime.resolve('build', 'index.html')
     app.use(
-      history(runtime.resolve('build', 'index.html'), {
+      history(htmlFile, {
         root: runtime.resolve('build'),
       })
     )
+    this.runtime.debug(`Using history fallback`, this.runtime.relative(htmlFile))
   } else if (typeof historyOptions === 'object') {
     let { htmlFile = runtime.resolve('build', 'index.html') } = historyOptions
 
     htmlFile = runtime.resolve(htmlFile)
+
+    this.runtime.debug(`Using history fallback`, this.runtime.relative(htmlFile))
 
     app.use(
       history(htmlFile, {
@@ -195,10 +243,13 @@ function setupStaticServer(app, staticOptions) {
 
   if (typeof staticOptions === 'string') {
     app.use(express.static(runtime.resolve(staticOptions)))
+    this.runtime.debug('serving static files', { staticOptions })
   } else if (staticOptions === true) {
     app.use(express.static(runtime.resolve('build')))
+    this.runtime.debug('serving static files from build/')
   } else if (typeof staticOptions === 'object') {
     app.use(express.static(runtime.resolve(staticOptions.path || staticOptions.root)))
+    this.runtime.debug('serving static files from', staticOptions)
   } else if (isArray(staticOptions)) {
     staticOptions.map(option => setupStaticServer.call(this, app, option))
   }
