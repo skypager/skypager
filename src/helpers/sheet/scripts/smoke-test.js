d46f0cfb491dd9aba1ab64ae1e66156a927358c6
soederpop <jon@dais.com>
2018-09-27 10:07:39 -0500

    updating smoke test

--- src/helpers/sheet/scripts/smoke-test.js
+++ src/helpers/sheet/scripts/smoke-test.js
@@ -1,5 +1,17 @@
 const runtime = require('@skypager/node')
-const serviceAccountPath = runtime.resolve('secrets', 'serviceAccount.json')
+const { mkdirSync, existsSync, writeFileSync } = require('fs')
+const { dirname, resolve } = require('path')
+
+const isCI = process.env.CI || process.env.JOB_NAME
+const serviceAccountPath =
+  process.env.GOOGLE_APPLICATION_CREDENTIALS ||
+  resolve(__dirname, '..', 'secrets', 'serviceAccount.json')
+const credentialExists = existsSync(serviceAccountPath)
+
+if (isCI && !credentialExists && process.env.SERVICE_ACCOUNT_DATA) {
+  !existsSync(dirname(serviceAccountPath)) && mkdirSync(dirname(serviceAccountPath))
+  writeFileSync(serviceAccountPath, process.env.SERVICE_ACCOUNT_DATA)
+}
 
 runtime.use(require('../lib'), {
   serviceAccount: serviceAccountPath,
