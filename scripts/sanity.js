1cb88df2c87b5ea599f35e598bb66ac067c31e85
soederpop <jon@dais.com>
2018-10-02 15:52:05 -0500

    output versions in appveyor

--- scripts/sanity.js
+++ scripts/sanity.js
@@ -1,5 +1,8 @@
 const runtime = require('@skypager/runtime')
 console.log({
+  runtimeVersion: require('@skypager/runtime/package.json').version,
+  nodeRuntimeVersion: require('@skypager/node/package.json').version,
+  fileManagerVersion: require('@skypager/features-file-manager/package.json').version,
   vm: runtime.vm,
   process: {
     title: process.title,
