439794ec30543de5024bb354f3a0c8968b2abaa5
soederpop <jon@dais.com>
2018-10-02 16:01:12 -0500

    sanity script is too early for this

--- scripts/sanity.js
+++ scripts/sanity.js
@@ -1,8 +1,5 @@
 const runtime = require('@skypager/runtime')
 console.log({
-  runtimeVersion: require('@skypager/runtime/package.json').version,
-  nodeRuntimeVersion: require('@skypager/node/package.json').version,
-  fileManagerVersion: require('@skypager/features-file-manager/package.json').version,
   vm: runtime.vm,
   process: {
     title: process.title,
