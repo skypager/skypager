1f2c8fd8dec0ad84cfb3937e61ac464af86b8ddd
soederpop <jon@dais.com>
2018-10-07 21:46:51 -0500

    update deploy script

--- src/examples/sheets-server/scripts/deploy.js
+++ src/examples/sheets-server/scripts/deploy.js
@@ -1,7 +1,8 @@
 const runtime = require('@skypager/node')
 const { spawn } = runtime.proc.async
+const { resolve } = require('path')
 const { clear, print, randomBanner } = runtime.cli
-const { debug = false, verbose } = runtime.argv
+const { nowToken = readNowToken(), debug = false, verbose } = runtime.argv
 
 clear()
 randomBanner(`Skypager`)
@@ -11,13 +12,30 @@ async function main() {
   print(`Deploying to now.sh`)
   await spawn(
     'now',
-    `-e SERVICE_ACCOUNT_DATA=@skypager-sheets-server-sa --npm ${debug ? '--debug' : ''}`.split(' '),
+    `--token ${nowToken} -e SERVICE_ACCOUNT_DATA=@skypager-sheets-server-sa --npm ${
+      debug ? '--debug' : ''
+    }`.split(' '),
     {
       stdio: verbose ? 'inherit' : 'ignore',
     }
   )
   print(`Aliasing deployment.`)
-  await spawn('now', ['alias'])
+  await spawn('now', ['--token', nowToken, 'alias'])
 }
 
 main()
+
+function readNowToken() {
+  if (process.env.NOW_TOKEN) {
+    return process.env.NOW_TOKEN
+  }
+
+  try {
+    return require(resolve(process.env.HOME, '.now.json')).token
+  } catch (error) {
+    console.log(
+      'Could not retrive NOW_TOKEN.  Please set the environment variabe, pass --now-token as an argv, or make sure you have a now.json file in your home directory.'
+    )
+    process.exit(1)
+  }
+}
