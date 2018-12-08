c368624e85fdeedd3a6b4712267302bfa12148ec
soederpop <jon@dais.com>
2018-12-08 00:15:45 -0600

    fixed serve script

--- src/apps/webapp/scripts/serve.js
+++ src/apps/webapp/scripts/serve.js
@@ -42,9 +42,7 @@ async function main() {
   }
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error)
-    process.exit(1)
-  })
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
