c95d6dda2091608338b523094563893deba1c710
soederpop <jon@dais.com>
2019-02-05 03:20:44 -0600

    api docs

--- src/runtimes/web/docs/api/features/asset-loaders.md
+++ src/runtimes/web/docs/api/features/asset-loaders.md
@@ -1,17 +1,82 @@
-<a name="unpkg"></a>
+<a name="AssetLoader"></a>
 
-## unpkg(dependencies, options)
+## AssetLoader ⇐ <code>Feature</code>
+The AssetLoader feature provides async helpers for injecting static asset elements into the page,
+as well as helpers for loading javascript packages into the global scope from unpkg.com
+
+**Kind**: global class  
+**Extends**: <code>Feature</code>  
+
+* [AssetLoader](#AssetLoader) ⇐ <code>Feature</code>
+    * [.image(url, [options])](#AssetLoader.image) ⇒ <code>PromiseLike.&lt;HtmlElement&gt;</code>
+    * [.css(url, [options])](#AssetLoader.css) ⇒ <code>PromiseLike.&lt;HtmlElement&gt;</code>
+    * [.stylesheet(url, [options])](#AssetLoader.stylesheet) ⇒ <code>PromiseLike.&lt;HtmlElement&gt;</code>
+    * [.script(url, [options])](#AssetLoader.script) ⇒ <code>PromiseLike.&lt;HtmlElement&gt;</code>
+    * [.unpkg(dependencies, options)](#AssetLoader.unpkg) ⇒ <code>Object.&lt;String, Object&gt;</code>
+
+<a name="AssetLoader.image"></a>
+
+### AssetLoader.image(url, [options]) ⇒ <code>PromiseLike.&lt;HtmlElement&gt;</code>
+Injects an image tag into the DOM
+
+**Kind**: static method of [<code>AssetLoader</code>](#AssetLoader)  
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| url | <code>String</code> |  | the url of the image |
+| [options] | <code>Object</code> | <code>{}</code> | options for element creation |
+
+<a name="AssetLoader.css"></a>
+
+### AssetLoader.css(url, [options]) ⇒ <code>PromiseLike.&lt;HtmlElement&gt;</code>
+Injects a stylesheet link tag into the DOM
+
+**Kind**: static method of [<code>AssetLoader</code>](#AssetLoader)  
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| url | <code>String</code> |  | the url of the stylesheet |
+| [options] | <code>Object</code> | <code>{}</code> | options for element creation |
+
+<a name="AssetLoader.stylesheet"></a>
+
+### AssetLoader.stylesheet(url, [options]) ⇒ <code>PromiseLike.&lt;HtmlElement&gt;</code>
+Injects a stylesheet link tag into the DOM
+
+**Kind**: static method of [<code>AssetLoader</code>](#AssetLoader)  
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| url | <code>String</code> |  | the url of the stylesheet |
+| [options] | <code>Object</code> | <code>{}</code> | options for element creation |
+
+<a name="AssetLoader.script"></a>
+
+### AssetLoader.script(url, [options]) ⇒ <code>PromiseLike.&lt;HtmlElement&gt;</code>
+Injects a script tag into the DOM
+
+**Kind**: static method of [<code>AssetLoader</code>](#AssetLoader)  
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| url | <code>String</code> |  | the url of the stylesheet |
+| [options] | <code>Object</code> | <code>{}</code> | options for element creation |
+
+<a name="AssetLoader.unpkg"></a>
+
+### AssetLoader.unpkg(dependencies, options) ⇒ <code>Object.&lt;String, Object&gt;</code>
 Load assets from unpkg.com by name, will asynchronously load them
 by injecting a script tag.  The Promise will resolve when the asset
 has been loaded.
 
-**Kind**: global function  
+**Kind**: static method of [<code>AssetLoader</code>](#AssetLoader)  
+**Returns**: <code>Object.&lt;String, Object&gt;</code> - returns an object whose keys are the global variable name, and whose values are the libraries that were injected  
 
 | Param | Type | Description |
 | --- | --- | --- |
-| dependencies | <code>object</code> | an object whose keys are the global variable name of the package,                                and the value is the path to the umd build on unpkg.com |
-| options | <code>object</code> | an options hash |
-| options.protocol | <code>string</code> | http or https |
+| dependencies | <code>Object</code> | an object whose keys are the global variable name of the package, |
+| options | <code>Object</code> | an options hash |
+| options.protocol | <code>String</code> | http or https |
 
 **Example**  
 ```js
--- src/runtimes/web/src/features/asset-loaders.js
+++ src/runtimes/web/src/features/asset-loaders.js
@@ -1,115 +1,165 @@
-export const shortcut = 'assetLoader'
+import runtime from '@skypager/web'
 
-export const featureMethods = ['image', 'stylesheet', 'script', 'css', 'unpkg', 'lazyInject']
-
-export function image(url, options = {}) {
-  return this.inject.img(url, options)
-}
-
-export function css(url, options = {}) {
-  return this.inject.css(url, options)
-}
-
-export function stylesheet(url, options = {}) {
-  return this.inject.css(url, options)
-}
-
-export function script(url, options = {}) {
-  return this.inject.js(url, options)
-}
+const { Feature } = runtime
 
 /**
- * Load assets from unpkg.com by name, will asynchronously load them
- * by injecting a script tag.  The Promise will resolve when the asset
- * has been loaded.
- *
- * @param {object} dependencies - an object whose keys are the global variable name of the package,
- *                                and the value is the path to the umd build on unpkg.com
- * @param {object} options - an options hash
- * @param {string} options.protocol - http or https
- *
- * @example
- *
- *  runtime.assetLoader.unpkg({
- *    React: 'react@16.7.0/umd/react.production.min.js'
- *  })
+ * @class AssetLoader
+ * @extends Feature
+ * @classdesc The AssetLoader feature provides async helpers for injecting static asset elements into the page,
+ * as well as helpers for loading javascript packages into the global scope from unpkg.com
  */
-export async function unpkg(dependencies = {}, { protocol = 'https' } = {}) {
-  const { entries, fromPairs } = this.lodash
+export default class AssetLoader extends Feature {
+  static shortcut = 'assetLoader'
 
-  const modules = await Promise.all(
-    entries(dependencies).map(([globalVariableName, packageName]) => {
-      const unpkgUrl = packageName.startsWith('http')
-        ? packageName
-        : `${protocol}://unpkg.com/${packageName}`
+  shortcut = 'assetLoader'
 
-      if (global[globalVariableName]) {
-        return [globalVariableName, global[globalVariableName]]
-      }
-
-      return this.inject.js(unpkgUrl).then(() => {
-        return [globalVariableName, global[globalVariableName]]
-      })
-    })
-  ).then(fromPairs)
-
-  return modules
-}
-
-export function lazyInject() {
-  // Function which returns a function: https://davidwalsh.name/javascript-functions
-  function _load(tag) {
-    return function(url, options = {}) {
-      // This promise will be used by Promise.all to determine success or failure
-      return new Promise(function(resolve, reject) {
-        var element = document.createElement(tag)
-        var parent = 'body'
-        var attr = 'src'
-
-        window.lastElement = element
-
-        // Important success and error for the promise
-        element.onload = function() {
-          console.log('loading', url)
-          resolve(url)
-        }
-        element.onerror = function(...args) {
-          console.log('error', url, ...args)
-          reject(url)
-        }
-
-        // Need to set different attributes depending on tag type
-        switch (tag) {
-          case 'script':
-            if (options.babel) {
-              element.type = 'text/babel'
-              // element['data-presets'] = 'es2015,stage-2,react'
-            }
-
-            if (options.async !== false) {
-              element.async = true
-            }
-
-            break
-          case 'link':
-            element.type = 'text/css'
-            element.rel = 'stylesheet'
-            attr = 'href'
-            parent = 'head'
-        }
-
-        // Inject into document to kick off loading
-        element[attr] = url
-        console.log('appending', element, document[parent])
-        const result = document[parent].appendChild(element)
-        console.log(result)
-      })
-    }
+  initialize() {
+    this.lazy('inject', () => this.lazyInject())
   }
 
-  return {
-    css: _load('link'),
-    js: _load('script'),
-    img: _load('img'),
+  /**
+   * Injects an image tag into the DOM
+   *
+   * @param {String} url - the url of the image
+   * @param {Object} [options={}] - options for element creation
+   * @returns {PromiseLike<HtmlElement>}
+   * @memberof AssetLoader
+   */
+  image(url, options = {}) {
+    return this.inject.img(url, options)
+  }
+
+  /**
+   * Injects a stylesheet link tag into the DOM
+   *
+   * @param {String} url - the url of the stylesheet
+   * @param {Object} [options={}] - options for element creation
+   * @returns {PromiseLike<HtmlElement>}
+   * @memberof AssetLoader
+   */
+  css(url, options = {}) {
+    return this.inject.css(url, options)
+  }
+
+  /**
+   * Injects a stylesheet link tag into the DOM
+   *
+   * @param {String} url - the url of the stylesheet
+   * @param {Object} [options={}] - options for element creation
+   * @returns {PromiseLike<HtmlElement>}
+   * @memberof AssetLoader
+   */
+  stylesheet(url, options = {}) {
+    return this.inject.css(url, options)
+  }
+
+  /**
+   * Injects a script tag into the DOM
+   *
+   * @param {String} url - the url of the stylesheet
+   * @param {Object} [options={}] - options for element creation
+   * @returns {PromiseLike<HtmlElement>}
+   * @memberof AssetLoader
+   */
+  script(url, options = {}) {
+    return this.inject.js(url, options)
+  }
+
+  /**
+   * Load assets from unpkg.com by name, will asynchronously load them
+   * by injecting a script tag.  The Promise will resolve when the asset
+   * has been loaded.
+   *
+   * @param {Object} dependencies - an object whose keys are the global variable name of the package,
+   * @param {Object} options - an options hash
+   * @param {String} options.protocol - http or https
+   * @returns {Object<String,Object>} returns an object whose keys are the global variable name, and whose values are the libraries that were injected
+   * @memberof AssetLoader
+   *
+   * @example
+   *
+   *  runtime.assetLoader.unpkg({
+   *    React: 'react@16.7.0/umd/react.production.min.js',
+   *    ReactDOM: 'react-dom@16.7.0/umd/react-dom.production.min.js'
+   *  })
+   */
+  async unpkg(dependencies = {}, { protocol = 'https' } = {}) {
+    const { entries, fromPairs } = this.lodash
+
+    const modules = await Promise.all(
+      entries(dependencies).map(([globalVariableName, packageName]) => {
+        const unpkgUrl = packageName.startsWith('http')
+          ? packageName
+          : `${protocol}://unpkg.com/${packageName}`
+
+        if (global[globalVariableName]) {
+          return [globalVariableName, global[globalVariableName]]
+        }
+
+        return this.inject.js(unpkgUrl).then(() => {
+          return [globalVariableName, global[globalVariableName]]
+        })
+      })
+    ).then(fromPairs)
+
+    return modules
+  }
+
+  lazyInject() {
+    // Function which returns a function: https://davidwalsh.name/javascript-functions
+    function _load(tag) {
+      return function(url, options = {}) {
+        // This promise will be used by Promise.all to determine success or failure
+        return new Promise(function(resolve, reject) {
+          var element = document.createElement(tag)
+          var parent = 'body'
+          var attr = 'src'
+
+          window.lastElement = element
+
+          // Important success and error for the promise
+          element.onload = function() {
+            console.log('loading', url)
+            resolve(url)
+          }
+          element.onerror = function(...args) {
+            console.log('error', url, ...args)
+            reject(url)
+          }
+
+          // Need to set different attributes depending on tag type
+          switch (tag) {
+            case 'script':
+              if (options.babel) {
+                element.type = 'text/babel'
+                // element['data-presets'] = 'es2015,stage-2,react'
+              }
+
+              if (options.async !== false) {
+                element.async = true
+              }
+
+              break
+            case 'link':
+              element.type = 'text/css'
+              element.rel = 'stylesheet'
+              attr = 'href'
+              parent = 'head'
+          }
+
+          // Inject into document to kick off loading
+          element[attr] = url
+          console.log('appending', element, document[parent])
+          const result = document[parent].appendChild(element)
+          console.log(result)
+        })
+      }
+    }
+
+    return {
+      css: _load('link'),
+      js: _load('script'),
+      img: _load('img'),
+    }
   }
 }
