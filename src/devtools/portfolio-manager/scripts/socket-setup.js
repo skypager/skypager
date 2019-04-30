0636c1e354b21bfbaec8a27cada170f302ae4f3e
soederpop <jon@dais.com>
2019-04-29 23:57:29 -0500

    updating runtime README, improvements to portfolio socket server to get around max socket message size with a shared file cache that uses integrity sha and cache key to pass large data buffers

--- src/devtools/portfolio-manager/scripts/socket-setup.js
+++ src/devtools/portfolio-manager/scripts/socket-setup.js
@@ -1,10 +1,21 @@
 module.exports = async function setupPortfolioManagerSocketServer(socket, runtime) {
+  const { enableVm } = runtime.argv
+
   runtime.use(require('@skypager/portfolio-manager'))
 
   await runtime.feature('portfolio-manager').enable()
   console.log('Starting Portfolio Manager')
   await runtime.fileManager.startAsync({ startPackageManager: true })
   await runtime.portfolio.startAsync()
+
+  socket.on('error', e => {
+    console.log('error', e.message)
+  })
+
+  createInvokeChannel(runtime.fileManager, '/file-manager/invoke')
+  createInvokeChannel(runtime.packageManager, '/package-manager/invoke')
+  createInvokeChannel(runtime.moduleManager, '/module-manager/invoke')
+  createInvokeChannel(runtime.portfolio, '/portfolio/invoke')
 
   socket.subscribe('/project-hashes/:projectName', projectId => {
     const projectName = projectId.startsWith(runtime.portfolio.scope)
@@ -30,7 +41,61 @@ module.exports = async function setupPortfolioManagerSocketServer(socket, runtim
       })
   })
 
+  if (enableVm) {
+    console.log('VM Enabled')
+    socket.subscribe(
+      '/vm/run-code',
+      ({ code, messageId, taskId = messageId || runtime.hashObject({ code }), ...options }) => {
+        const replyChannel =
+          options.replyChannel || `/vm/run-code/response/${taskId.replace(/\W/g, '')}`
+
+        console.log('VM Got Task', { taskId, code, replyChannel })
+
+        runCode(code, options)
+          .then(({ cacheKey, integrity }) => {
+            return socket.publish(replyChannel, { cacheKey, integrity, error: false })
+          })
+          .catch(error => socket.publish(replyChannel, { result: error, error: true }))
+      }
+    )
+  }
+
   console.log('Finished.')
+
+  async function runCode(code, options = {}) {
+    const { refresh = false } = options
+
+    const cacheKey = runtime.hashObject({
+      code,
+      ...(refresh && { cacheBuster: Math.random() }),
+    })
+
+    const inCache = await runtime.fileManager.cache.get(cacheKey).catch(e => false)
+
+    if (inCache) {
+      runtime.debug('portfolio socket vm cache hit', {
+        cacheKey,
+        integrity: inCache.integrity,
+        code,
+      })
+
+      return {
+        cacheKey,
+        integrity: inCache.integrity,
+      }
+    } else {
+      runtime.debug('portfolio socket vm cache miss', {
+        cacheKey,
+        code,
+      })
+
+      const response = await runtime.scriptRunner.runCode(code, options)
+      const result = runtime.lodash.cloneDeep(response.result)
+      const integrity = await runtime.fileManager.cache.put(cacheKey, JSON.stringify(result))
+      return { cacheKey, integrity }
+    }
+  }
+
   async function getProjectHash(projectName) {
     const response = await runtime.portfolio.hashProjectTrees({ projects: [projectName] })
     return response && response[projectName] ? response[projectName] : response
@@ -39,5 +104,54 @@ module.exports = async function setupPortfolioManagerSocketServer(socket, runtim
   async function getProjectBuildHash(projectName) {
     const response = await runtime.portfolio.hashBuildTree(projectName)
     return response
+  }
+
+  function createInvokeChannel(host, prefix) {
+    socket.subscribe(prefix, ({ refresh = false, method = 'get', args = [], messageId }) => {
+      if (!messageId) {
+        return
+      }
+
+      const responseKey = `${prefix}/response/${messageId}`
+      const cacheKey = runtime.hashObject({
+        prefix,
+        responseKey,
+        method,
+        args,
+        ...(refresh && { cacheBuster: Math.random() }),
+      })
+
+      runtime.fileManager.cache
+        .get(cacheKey)
+        .then(({ metadata = {}, integrity = '' }) => {
+          runtime.debug('portfolio socket cache hit', {
+            cacheKey,
+            integrity,
+            method,
+            args,
+            host: host.name,
+            uuid: host.uuid,
+          })
+          socket.publish(responseKey, {
+            metadata,
+            integrity,
+          })
+        })
+        .catch(error => {
+          if (error.code === 'ENOENT') {
+            runtime.debug('portfolio socket cache miss', {
+              cacheKey,
+              method,
+              args,
+              host: host.name,
+              uuid: host.uuid,
+            })
+
+            Promise.resolve(host.invoke(method, ...args))
+              .then(result => runtime.fileManager.cache.put(cacheKey, JSON.stringify(result)))
+              .then(integrity => socket.publish(responseKey, { integrity: integrity.toString() }))
+          }
+        })
+    })
   }
 }
--- src/runtime/README.md
+++ src/runtime/README.md
@@ -18,6 +18,135 @@ In addition to these base framework classes, it provides a number of utilities u
 - [Array Query](src/utils/query.js)
 - [String Utils](src/utils/string.js)
 
+## Runtimes
+
+A Runtime is a class which provides a global singleton instance that can be shared by all modules in a project.  
+
+This global singleton instance is an observable state machine, with an event emitter, a vm, and helper module registries.
+
+As a developer, you should be building everything that your projects have in common into runtime bundle that can be cached long term, 
+and ideally shared between many projects. 
+
+## Helpers
+
+Helpers are a core component of the skypager framework.  They are used to define module types and categories, usually matching the plural names found in the the source code organization conventions.
+
+For example, in one of your apps, you might have the following structure:
+
+```
+src
+├── apps
+│   ├── PublicApp.js
+│   └── UserApp.js
+├── components
+│   ├── AddItem.js
+│   ├── ItemList.js
+│   ├── NavButton.js
+├── features
+│   ├── items.js
+│   ├── permissions.js
+│   ├── authentication.js
+├── pages
+│   ├── HomePage
+│   │   └── index.js
+│   ├── ItemPage
+│   │   └── index.js
+```
+
+In this app, you might have the following `Helper` subclasses
+
+```javascript
+import { Helper } from '@skypager/runtime'
+
+class App extends Helper { }
+class Component extends Helper { }
+class Feature extends Helper { }
+class Page extends Helper { }
+```
+
+Which get used in the following way
+
+```javascript
+// src/runtime.js 
+import runtime from '@skypager/runtime'
+import App from 'helpers/App'
+import Component from 'helpers/Component'
+import Page from 'helpers/Page'
+import PublicApp from 'apps/PublicApp'
+import UserApp from 'apps/UserApp'
+import HomePage from 'pages/HomePage'
+import ItemPage from 'pages/ItemPage'
+import ItemsFeature from 'features/items'
+import PermissionsFeature from 'features/permissions'
+import AuthenticationFeature from 'features/authentication'
+
+runtime
+  .use(App)
+  .use(Component)
+  .use(Page)
+
+runtime.apps.register('public-app', () => PublicApp)
+runtime.apps.register('user-app', () => UserApp)
+runtime.pages.register('HomePage', () => HomePage)
+runtime.features.register('items', () => ItemsFeature)
+runtime.features.register('permissions', () => PermissionsFeature)
+runtime.features.register('authentication', () => AuthenticationFeature)
+
+// Enable all of our features and export the runtime.  The Module require.cache
+// will ensure that we always have the same instance anywhere it is imported in the project
+export default runtime
+  .use('permissions')
+  .use('authentication', {
+    provider: 'oauth2',
+    config: { token: "abc", callbackUrl: "/callback" }
+  })
+  .use('items')
+```
+
+The `Helper` class provides you with a way for defining object models on top of the different types of modules in your project.  
+
+And once the runtime users a `Helper` (e.g. `runtime.use(App)`) you can register the available instances of these modules with module registries (such as `runtime.apps`, `runtime.features` and `runtime.pages` in the example above.)
+
+With these systems in place, it is hopefully clearer how you might define an `App` as a collection of `Pages` and a `Page` as a collection of `Components` which use different `Features`.
+
+Since an `App`, `Component`, `Feature`, or `Page` all have their own life cycles and configuration mechanisms, but common to each other, the `Helper` gives you a place to define what all `Features` `Pages` and `Apps` have in common in your project.
+
+By modeling this logic in helper classes, you can better take advantage of declarative coding styles to compose all of the various pieces of your application together. 
+
+Take a look at the example below. 
+
+```javascript
+// the module we wrote above for example
+import runtime from './runtime'
+
+const MyWebsite  = () => {
+  const UserApp = runtime
+    .app('UserApp', {
+      pages: {
+        "/" : "HomePage",
+        "/items": "ItemsPage",
+        "/items/:id": "ItemsPage"
+      } 
+    })  
+}
+
+let mySite
+
+async function main() {
+  await runtime.start()
+  mySite = MyWebsite()
+
+  return mySite.render()
+}
+
+main()
+```
+
+In this application, we're building an app by describing which pages it has and which features it depends on.
+
+The `Helper` class is how we describe how these different types of pieces fit together.
+
 ## API Documentation
 
 [Read the API Documentation](docs/api)
+
