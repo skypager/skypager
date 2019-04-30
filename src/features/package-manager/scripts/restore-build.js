6ce7ab22f99edf53f9cc57c1910577d4bce3778b
soederpop <jon@dais.com>
2019-04-29 21:35:19 -0500

    rename restore script to restore-build

--- src/features/package-manager/scripts/restore-build.js
+++ src/features/package-manager/scripts/restore-build.js
@@ -1,15 +1,7 @@
 const runtime = require('@skypager/node')
 
-const { currentPackage, fileManager, packageManager, moduleManager, argv } = runtime
+const { fileManager, packageManager, git, argv } = runtime
 const { colors, print } = runtime.cli
-const { castArray, uniq } = runtime.lodash
-
-const noYarnCache =
-  argv.noCache || argv.cache === false || argv.noYarnCache || argv.yarnCache === false
-
-const buildFolders = uniq(['dist', 'lib', 'build'].concat(argv._))
-  .concat(castArray(argv.buildFolder))
-  .filter(v => v && v.length)
 
 main()
   .then(() => {
@@ -33,10 +25,41 @@ async function main() {
     downloadCommand = 'skypager download-from-npm',
   } = argv
 
+  if (fileManager.packageLocations.length === 1) {
+    if (git.modifiedSince().length) {
+      print(`Package has been modified. Running build command`)
+    } else {
+      print(`Package has not been modified. Running download command`)
+    }
+  }
+
+  const { difference } = runtime.lodash
+
   await fileManager.startAsync({ startPackageManager: true })
 
-  await Promise.all([])
+  const tag = runtime.git.findLatestTag()
+  const packageNames = packageManager.packageData
+    .filter(
+      pkg =>
+        pkg &&
+        pkg.name &&
+        !pkg._file.dir.match(/(test|spec).fixtures/) &&
+        (argv.includeRoot || pkg._file.dir !== runtime.gitInfo.root)
+    )
+    .map(p => p.name)
+
+  const changedPackages = await packageManager.selectModifiedPackages({ since: tag, names: true })
+
+  const downloadList = difference(packageNames, changedPackages)
+  const buildList = changedPackages
+
+  print(`Running download command: ${downloadCommand} in`)
+  print(downloadList.map(n => `- ${n}`), 4)
+  print(`Running build command: ${buildCommand} in`)
+  print(buildList.map(n => `- ${n}`), 4)
 }
+
+main()
 
 function displayHelp() {
   runtime.cli.randomBanner('Skypager')
@@ -49,6 +72,7 @@ function displayHelp() {
   --download-command <cmd>     package.json script, or shell command to download the build artifacts
   --build-manifest <filename>  the name of the json file which contains the build manifest that includes 
                                a sourceHash property
+  --include-root               by default, we'll skip the root package in a monorepo. pass true to include it anyway.
   `.trim()
   )
 }
