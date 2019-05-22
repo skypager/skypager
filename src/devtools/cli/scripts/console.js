3db8ea7a4f0300b066341fd7f5ab86df22633eb4
soederpop <jon@dais.com>
2019-05-22 02:24:46 -0500

    add --dev-mode flag to skypager console, which starts commonly used dev modules

--- src/devtools/cli/scripts/console.js
+++ src/devtools/cli/scripts/console.js
@@ -1,3 +1,55 @@
 const runtime = require('@skypager/node')
 
-runtime.repl('interactive').launch({ runtime })
+async function main() {
+  const requireScripts = runtime.lodash.castArray(runtime.argv.require).filter(Boolean)
+
+  if (requireScripts.length) {
+    for (let script of requireScripts) {
+      await runtime.scriptRunner.runScriptAtPath(runtime.resolve(script))
+    }
+  }
+
+  if (runtime.argv.devMode) {
+    await runtime.fileManager.startAsync({ startPackageManager: true }).catch(error => error)
+
+    try {
+      runtime.use(require('@skypager/helpers-document'))
+    } catch (error) {
+      console.error(error)
+    }
+
+    const serviceAccount = runtime.resolve('secrets', 'serviceAccount.json')
+
+    if (runtime.fsx.existsSync(serviceAccount)) {
+      try {
+        runtime.use(require('@skypager/helpers-sheet'), {
+          serviceAccount,
+          projectId: require(serviceAccount).project_id,
+        })
+      } catch (error) {
+        console.error(error)
+      }
+
+      try {
+        runtime.use(require('@skypager/helpers-google-doc'), {
+          serviceAccount,
+          projectId: require(serviceAccount).project_id,
+        })
+      } catch (error) {}
+    }
+
+    try {
+      runtime.use(require('@skypager/features-webpack'))
+    } catch (error) {}
+  }
+
+  runtime.repl('interactive').launch({
+    runtime,
+    skypager: runtime,
+    lodash: runtime.lodash,
+    fileManager: runtime.fileManager,
+    packageManager: runtime.packageManager,
+  })
+}
+
+main()
