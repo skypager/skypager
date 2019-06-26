acde1549498d4daf7ad22d0fdf8bfdfe9aa3622a
soederpop <jon@dais.com>
2019-06-26 13:37:26 -0500

    converting google integration to class

---
+++ src/features/google/src/GoogleIntegration.js
@@ -0,0 +1,412 @@
+import { Feature } from '@skypager/runtime'
+import { google as g } from 'googleapis'
+
+export default class GoogleIntegration extends Feature {
+  static shortcut = 'google'
+  static isObservable = true
+
+  static optionTypes = {
+    googleProject: 'string',
+    serviceAccount: 'string',
+    driveVersion: 'string',
+    docsVersion: 'string',
+  }
+
+  /**
+   * Returns an array of objects compatible with creating instances of the GoogleSheet helper
+   * provided by @skypager/helpers-sheet.  These objects contain useful metadata about the google spreadsheet,
+   * without yet incurring the cost of loading all of the cells.
+   *
+   * Accepts the same arguments as listFiles, but hard codes the mimeType to the google-apps.spreadsheet
+   */
+  async listSpreadsheets(options = {}) {
+    const { maxResults = 100, sharedWithMe = true } = options
+
+    const { drive } = this
+
+    let query = "trashed=false and mimeType='application/vnd.google-apps.spreadsheet'"
+
+    if (sharedWithMe) {
+      query = `${query} and sharedWithMe`
+    }
+
+    if (typeof options.query === 'string') {
+      query = `and (${options.query})`
+    }
+
+    const teamDriveOptions =
+      options.teamDriveOptions ||
+      (options.teamDrives !== false
+        ? { supportsTeamDrives: true, includeTeamDriveItems: true }
+        : {})
+
+    const files = await drive.files
+      .list({ maxResults, q: query, ...teamDriveOptions })
+      .then(r => r.data)
+
+    const { pick } = this.lodash
+
+    const records = files.items.map(i => ({
+      ...pick(i, 'id', 'title', 'modifiedDate', 'lastModifyingUserName'),
+      sheetId: i.id,
+      ...{ getDriveFile: () => i },
+    }))
+
+    return records
+  }
+
+  /**
+   * Lists all files with the google apps folder mime type.
+   */
+  async listFolders(options = {}) {
+    return this.listFiles({
+      ...options,
+      mimeType: 'application/vnd.google-apps.folder',
+    })
+  }
+
+  /**
+   * Returns a list of file objects from the google drive files API.  Is a convenience wrapper for constructing
+   * the query syntax more easily. See https://developers.google.com/drive/api/v3/search-files for the exact parameters.
+   *
+   * @param {Object} options
+   * @param {Number} [options.maxResults=100] the maximum number of results to show
+   * @param {Boolean} [options.sharedWithMe=true] return files that are shared with you, in addition to ones you own
+   * @param {Boolean} [options.teamDrives=true] return files that are shared with you in team drives, in addition to ones you own
+   * @param {Boolean} [options.handleResponse=false] useful for debugging purposes to see the full axios response object from google's rest API
+   * @param {Object} [options.teamDriveOptions] specify your own team drive options
+   * @param {Boolean} [options.trashed=false] include trashed files in the results
+   * @param {String} [options.parentsOperator='and'] which operator to use when using the parents parameter
+   * @param {Array} [options.parents=[]] parent folder ids to search
+   * @param {String} [options.mimeType] limit the search by mimeType, useful for finding google docs, vs sheets, folders, etc
+   * @param {String} [options.and] an additional query condition that must return match
+   * @param {String} [options.or] an additional query condition that can also return match
+   */
+  async listFiles(options = {}) {
+    const { drive } = this
+    const {
+      maxResults = 100,
+      sharedWithMe = true,
+      trashed = false,
+      mimeType,
+      and,
+      or,
+      parents = [],
+      parentsOperator = 'and',
+    } = options
+
+    let query = trashed ? `trashed=true` : `trashed=false`
+
+    if (sharedWithMe) {
+      query = `${query} and sharedWithMe`
+    }
+
+    if (mimeType) {
+      query = `${query} and mimeType='${mimeType}'`
+    }
+
+    if (parents && parents.length) {
+      const parentConditions = parents.map(id => `'${id}' in parents`)
+      query = `${query} ${parentsOperator} (${parentConditions.join(' or ')})`
+    }
+
+    if (typeof and === 'string') {
+      query = `${query} and ${and}`
+    }
+
+    if (typeof or === 'string') {
+      query = `${query} or ${or}`
+    }
+
+    const teamDriveOptions =
+      options.teamDriveOptions ||
+      (options.teamDrives !== false
+        ? { supportsTeamDrives: true, includeTeamDriveItems: true }
+        : {})
+
+    const response = await drive.files.list({
+      maxResults,
+      q: query,
+      ...teamDriveOptions,
+    })
+
+    if (options.handleResponse) {
+      return response
+    }
+
+    let files = response.data.items || response.data.files || []
+
+    if (mimeType && mimeType.length) {
+      files = files.filter(i => String(i.mimeType).toLowerCase() === mimeType.toLowerCase())
+    }
+
+    return files
+  }
+
+  async listDocuments(options = {}) {
+    const { pick } = this.lodash
+    const { parents = [], recursive = false } = options
+    const { docs } = this
+
+    if (recursive) {
+      const folders = await this.listFolders()
+      const parentIds = folders.map(f => f.id)
+      parents.push(...parentIds)
+    }
+
+    const files = await this.listFiles({
+      parents,
+      // finding files in folders that are sharedWithMe
+      parentsOperator: 'or',
+      ...options,
+      mimeType: 'application/vnd.google-apps.document',
+    })
+
+    const records = files.map(i => ({
+      ...pick(i, 'id', 'title', 'modifiedDate', 'lastModifyingUserName'),
+      documentId: i.id,
+      ...{
+        getDocument: () => docs.documents.get({ documentId: i.id }).then(resp => resp.data),
+        getDriveFile: () => i,
+      },
+    }))
+
+    return records
+  }
+  /**
+   * The initial state of the google integration can be passed as options when creating the feature
+   * e.g runtime.feature('google', { driveVersion: 'v3' }) and certain flags can be set by the
+   * node script's process.argv --flags e.g. --drive-version=v3 --google-project=your-project-id
+   *
+   * the state's settings object is used for configuring the version numbers of various services to use,
+   * as well as for providing the server to server auth credentials in the form of a path to a service account JSON.
+   *
+   * This is a standard hook function in the Skypager Helper class, and is used to set the initial value of the mobx
+   * observable state object.
+   */
+  initialState() {
+    const { pick } = this.lodash
+
+    return {
+      googleReady: false,
+      googleError: false,
+      settings: {
+        ...pick(
+          this.runtime.argv,
+          'googleProject',
+          'serviceAccount',
+          'driveVersion',
+          'docsVersion'
+        ),
+        ...pick(
+          this.options,
+          'googleProject',
+          'serviceAccount',
+          'driveVersion',
+          'docsVersion',
+          'versions'
+        ),
+        ...(this.options.settings || {}),
+      },
+    }
+  }
+
+  /**
+   * Returns a map of specific versions to use of a google api.
+   * Drive is hard coded to v2 for compatibility with the @skypager/helpers-sheet and @skypager/helpers-google-doc libraries.
+   * Anything not specified will be v1
+   */
+  get versions() {
+    return {
+      drive: this.settings.driveVersion || 'v2',
+      ...this.settings.versions,
+    }
+  }
+
+  /**
+   * The settings are used for authentication and version selection.  Settings will be injected
+   * when the feature is enabled, or based on how the feature is initialized when it is created.
+   * Some settings can be picked up from the node.js process.argv --flags
+   */
+  get settings() {
+    return this.state.get('settings') || {}
+  }
+
+  /**
+   * Returns the version number of the google service you want to interact with.
+   *
+   * drive is defaulted to v2 for example.
+   */
+  serviceVersion(apiName) {
+    return this.versions[apiName] || 'v1'
+  }
+
+  /**
+   * Creates an axios client for interacting with the google apis for the given apiName
+   *
+   * @param {String} apiName the name of the google api, e.g. drive
+   * @param {Object} options optons to pass
+   * @param {Object} [options.auth=this.auth] the authenticated client to call on behalf of
+   * @param {String} [options.version='v1'] the version of the service you want to interact with
+   *
+   * @returns {Object}
+   */
+  service(apiName, options = {}) {
+    const { auth = this.auth } = options
+    const svc = g[apiName]
+
+    if (!options.version) {
+      options.version = this.serviceVersion(apiName)
+    }
+
+    return svc({ auth, ...options })
+  }
+
+  /**
+   * Returns all of the googleapis from their node.js sdk
+   */
+  get apis() {
+    return g
+  }
+
+  /**
+   * Returns an axios client for talking to the google drive service.
+   * defaults to v2 of that api.
+   */
+  get drive() {
+    const { driveVersion = this.serviceVersion('drive') } = this.settings
+    return this.service('drive', {
+      version: driveVersion,
+    })
+  }
+
+  /**
+   * Returns an axios client for talking to the google docs api.
+   * defaults to v1 of that api.
+   */
+  get docs() {
+    const { docsVersion = this.serviceVersion('docs') } = this.settings
+    return this.service('docs', {
+      version: docsVersion,
+    })
+  }
+
+  /**
+   * Returns true if there are errors initializing the google api client.
+   * Errors are usually related to authentication and service account credentials.
+   */
+  get hasErrors() {
+    return !!this.state.get('googleError')
+  }
+
+  /**
+   * Returns a promise which will resolve when the google api client has its authentication
+   * setup and ready to go.
+   *
+   * @returns {Promise}
+   */
+  whenReady() {
+    return new Promise((resolve, reject) => {
+      if (this.hasErrors) {
+        reject(new Error(`The Google Integration failed`))
+      } else if (this.state.get('googleReady')) {
+        resolve(this)
+      } else {
+        const dispose = this.state.observe(update => {
+          if (update.name === 'googleError' && update.newValue) {
+            reject(new Error('The Google Integration failed'))
+          } else if (update.name === 'googleReady' && update.newValue) {
+            dispose()
+            resolve(this)
+          }
+        })
+      }
+    })
+  }
+
+  /**
+   * This lifecycle hook of the Skypager Feature class will get called when
+   * the runtime says runtime.feature('google').enable().  By the time this is called,
+   * the feature will expect to know where to find the service account JSON on disk,
+   * and which google project you expect to be talking to.
+   */
+  async featureWasEnabled(options = {}) {
+    this.state.set('settings', {
+      ...(this.state.get('settings') || {}),
+      googleProject: process.env.GCLOUD_PROJECT,
+      serviceAccount:
+        process.env.GOOGLE_APPLICATION_CREDENTIALS ||
+        this.runtime.resolve('secrets', 'serviceAccount.json'),
+      ...options,
+    })
+
+    await this.initializeGoogleAPI()
+  }
+
+  /**
+   * Initializes the google API client and caches the authenticated client on this.auth
+   *
+   * @param {Object} options
+   * @param {String} [options.serviceAccount=process.env.GOOGLE_APPLICATION_CREDENTIALS] the path to the service account
+   * @param {String} [options.googleProject=process.env.GCLOUD_PROJECT] he google project you're talking to
+   */
+  async initializeGoogleAPI(options = {}) {
+    const {
+      googleProject = process.env.GCLOUD_PROJECT,
+      serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    } = {
+      ...this.settings,
+      ...options,
+    }
+
+    // These values need to be set for the node.js sdk to work
+    process.env.GCLOUD_PROJECT = googleProject
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = serviceAccount
+
+    if (!googleProject) {
+      const e = new Error(
+        `Missing GCLOUD_PROJECT please specify it as an environment variable, or with the --google-project command line flag`
+      )
+      console.error(e)
+      this.state.set('googleProjectError', e.message)
+      this.state.set('googleError', true)
+      this.emit('googleFailed', e)
+    }
+
+    if (!serviceAccount) {
+      const e = new Error(
+        `Missing GOOGLE_APPLICATION_CREDENTIALS. Please specify the path to your service account json file as an environment variable, or by using the --service-account command line flag`
+      )
+      console.error(e)
+      this.state.set('googleCredentialsError', e.message)
+      this.state.set('googleError', true)
+      this.emit('googleFailed', e)
+    }
+
+    const serviceAccountExists = await this.runtime.fsx.existsAsync(serviceAccount)
+
+    if (!serviceAccountExists) {
+      const e = new Error(`Could not find service account json at ${serviceAccount}`)
+      console.error(e)
+      this.state.set('googleCredentialsError', e.message)
+      this.state.set('googleError', true)
+      this.emit('googleFailed', e)
+    }
+
+    const auth = await this.createAuthClient()
+    this.hide('auth', auth)
+    this.state.set('googleReady', true)
+    this.emit('googleIsReady', this)
+  }
+
+  async createAuthClient(options = {}) {
+    const {
+      scopes = [
+        'https://www.googleapis.com/auth/drive.readonly',
+        'https://www.googleapis.com/auth/drive.metadata.readonly',
+      ],
+    } = options
+
+    return g.auth.getClient({ ...options, scopes })
+  }
+}
